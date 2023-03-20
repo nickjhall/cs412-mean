31e3dae9f02ed26bf04ef700be28e84d9b9dea00
nickjhall <njhall@bu.edu>
2023-03-19 22:34:32 -0400

    Create promise route

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 let routes = require('./ps4.js');
 
 app.set('view engine', 'pug');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/ps4', routes);
 
 app.listen(3000, () => {
--- ps4.js
+++ ps4.js
@@ -1,7 +1,30 @@
 let express = require('express');
+let request = require('request');
 let routes = express.Router();
 
 routes.get('/', (req, res) => {
+    res.render('index', {});
+});
+
+routes.post('/promise', (req, res) => {
+    new Promise((resolve, reject) => {
+        const queryTerms = encodeURIComponent(req.body.promise);
+        const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + process.env.TMBD_KEY + "&query=" + queryTerms;
+        request({url: url, json: true}, (err, response, body) => {
+            resolve(body);
+        })
+    }).then((result) =>
+    {
+        console.log("Movie data success");
+        res.render('results', {queryTerms: req.body.promise, movieData: result.results});
+    });
+});
+
+routes.post('/async', (req, res) => {
+    res.render('index', {});
+});
+
+routes.post('/callback', (req, res) => {
     res.render('index', {});
 });
 
